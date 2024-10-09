efb529e7b79da248ae757b66c54734f6295be09c
Erick Vicencio Del Angel <erickvicencio@gmail.com>
2024-10-08 21:54:31 -0700

    add theme and stuff

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neorg/neorg",
+  lazy=false,
+  version="*",
+  ft="norg",
+  config = function ()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer" ] = {}
+      }
+    })
+    
+  end
+}
